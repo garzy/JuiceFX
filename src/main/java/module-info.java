fefe17d66efdda80e3298e805cee25de1c4fe630
GaRzY <dagarill@gmail.com>
2019-08-17 16:51:14 +0200

    module-info package fixes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,31 +1,26 @@
 /*
- * #%L
- * afterburner.fx
- * %%
- * Copyright (C) 2013 - 2019 Adam Bien
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * #%L afterburner.fx %% Copyright (C) 2013 - 2019 Adam Bien %% Licensed under
+ * the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License. #L%
  */
 module JuiceFX {
     requires javafx.controls;
     requires javafx.fxml;
     requires javax.inject;
-    requires java.annotation;
+    requires java.compiler;
     requires com.google.guice;
 
-    opens org.openjfx to javafx.fxml;
     exports com.airhacks.afterburner.injection;
+    exports com.airhacks.afterburner.injection.modules;
     exports com.airhacks.afterburner.configuration;
     exports com.airhacks.afterburner.views;
 }
